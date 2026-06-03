6c17dcf2705b91a8a9420356d87314d9c2ca179a
Saif Ullah Arshad <saifullaharshad110@gmail.com>
2026-06-03 18:10:00 +0500

    Add score method to RandomForestClassifier

--- src/random_forest.py
+++ src/random_forest.py
@@ -115,3 +115,5 @@ class RandomForestClassifier:
     def score(self, X, y):
         return float(sum(self.predict(X) == y) / len(y))
 # TODO: add predict_proba for soft voting in future
+    def score(self, X, y):
+        return float(sum(self.predict(X) == y) / len(y))
